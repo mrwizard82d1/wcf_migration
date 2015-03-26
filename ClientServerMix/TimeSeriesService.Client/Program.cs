9f8cd05e60865115506bf4fee2793804fed60b3a
Larry Jones <mrwizard82d1@gmail.com>
2015-03-26 15:52:06 -0500

    Add (console) client.
    Not done yet. The console runs, but it never queries the service.

---
+++ ClientServerMix/TimeSeriesService.Client/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TimeSeriesService.Client
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Press <ENTER> to terminate client.");
+            Console.ReadLine();
+        }
+    }
+}
