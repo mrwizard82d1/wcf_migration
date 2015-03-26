c9ae58548d14571ce2e3b8ff5449d9172864ee7f
Larry Jones <mrwizard82d1@gmail.com>
2015-03-26 14:07:00 -0500

    Add (console) host application.
    Although the code builds successully, if you try to run it, it throws an
    exception because no endpoints have been configured in the `App.config`
    file.
    
    You have been warned!

---
+++ ClientServerMix/TimeSeriesService.Host/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace TimeSeriesService.Host
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (var host = new ServiceHost(typeof(TimeSeriesServiceImpl)))
+            {
+                host.Open();
+
+                Console.WriteLine("Pres <ENTER> to terminate host.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
