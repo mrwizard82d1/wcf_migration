f5625f546aa0f1e0780c0a842b93e56614820083
Larry Jones <mrwizard82d1@gmail.com>
2015-03-26 10:06:54 -0500

    Repair (future) issues with solution.
    These errors manifested themselves as obscure warnings on the generated
    'app.config` file. These warnings indicated that both the `name`
    attribute of the `service` element and the `contract` attribute of the
    `endpoint` element were invalid.
    
    These names were misspelled. I had inadvertently (ha ha) renamed my
    concrete contract implementation to `TimesSeriesServiceImpl`. (Did you
    notice it began with `Time**s**`? I did not for the longest time.) In
    addition, at some point, I believe I had the namespace name,
    `TimeSeriesService`, incorrect in the same way.
    
    I repaired the `app.config` warning by copying names from the source
    code files. When I began my check-in process, I then noticed the
    `Time**s**` error. I repaired that error by renaming my classes (and my
    source files).

---
+++ ClientServerMix/TimeSeriesService/TimeSeriesServiceImpl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using TimeSeries;
+
+namespace TimeSeriesService
+{
+    public class TimeSeriesServiceImpl : ITimeSeriesService
+    {
+        public TimeSeriesBase New(IEnumerable<IDataPoint> dataPoints)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
