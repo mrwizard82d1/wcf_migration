7deffcb1b9c1ad202dbda734c2df241d7b92d48a
Larry Jones <mrwizard82d1@gmail.com>
2015-03-27 13:00:45 -0500

    Add additional functional test cases.
    Add code to test for two data points (returning a `RegularTimeSeries`)
    and for three data points (returning a `SetPointTimeSeries`).

--- ClientServerMix/TimeSeriesService.Client/Program.cs
+++ ClientServerMix/TimeSeriesService.Client/Program.cs
@@ -15,8 +15,29 @@ namespace TimeSeriesService.Client
                 new DataPoint()
             }.ToArray();
             var oneDataPointResult = proxy.New(oneDataPoint);
-            Console.WriteLine("Irregular: {0}", oneDataPointResult);
+            Console.WriteLine("One data point: {0}", oneDataPointResult);
 
+            var twoDataPoints = new List<DataPoint>
+            {
+                new DataPoint(),
+                new DataPoint()
+            }.ToArray();
+            var twoDataPointsResult = proxy.New(twoDataPoints);
+            Console.WriteLine("Two data points: {0}", twoDataPointsResult);
+
+            var threeDataPoints = new List<DataPoint>
+            {
+                new DataPoint(),
+                new DataPoint(),
+                new DataPoint()
+            }.ToArray();
+            var threeDataPointsResult = proxy.New(threeDataPoints);
+            Console.WriteLine("Three data points: {0}",
+                threeDataPointsResult);
+
+            Console.WriteLine();
+            Console.WriteLine("--");
+            Console.WriteLine();
             Console.WriteLine("Press <ENTER> to terminate client.");
             Console.ReadLine();
         }
