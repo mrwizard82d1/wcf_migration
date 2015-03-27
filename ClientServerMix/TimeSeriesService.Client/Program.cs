a9cc370c7fcfde20a0e8fa6a0f02d4bf2cc8e13f
Larry Jones <mrwizard82d1@gmail.com>
2015-03-27 12:50:22 -0500

    Add TimeSeriesService client.
    Finally. It works!
    
    I do not quite understand the sequence of incantations that I used to
    make this particular "instantiation" work. Perhaps I was simply more
    careful.
    
    I can think of two different actions that I have undertaken. First, I
    added the reference to the `TimeSeries.dll` (and built the project)
    **before** generating the service references. Second, I did not create a
    "complex" name space for the service reference. (Perhaps my original
    namespace, sharing part of the namespace "tree" with other projects,
    inadvertently included the code in `TimeSeries`.)
    
    It is now working. Time to test other concrete implementations of
    `TimeSeriesBase`.

--- ClientServerMix/TimeSeriesService.Client/Program.cs
+++ ClientServerMix/TimeSeriesService.Client/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using TimeSeries;
+using TimeSeriesService.Client.TimeSeriesReference;
 
 namespace TimeSeriesService.Client
 {
@@ -6,6 +9,14 @@ namespace TimeSeriesService.Client
     {
         static void Main()
         {
+            var proxy = new TimeSeriesServiceClient();
+            var oneDataPoint = new List<DataPoint>
+            {
+                new DataPoint()
+            }.ToArray();
+            var oneDataPointResult = proxy.New(oneDataPoint);
+            Console.WriteLine("Irregular: {0}", oneDataPointResult);
+
             Console.WriteLine("Press <ENTER> to terminate client.");
             Console.ReadLine();
         }
